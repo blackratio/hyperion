9774d7811edf909be454c67579ea59b1c10e2b8d
blackratio <blackratio@gmail.com>
2016-04-19 23:45:01 +0200

    hurry up

--- app/utils/routeprovider.js
+++ app/utils/routeprovider.js
@@ -1,23 +1,23 @@
 (function() {
 
-   'use strict';
+    'use strict';
 
-   angular
-      .module('initRouter', [
-         'ui.router',
-         'conf.interceptors'
-      ])
-      .config(initProvider);
+    angular
+        .module('initRouter', [
+            'ui.router',
+            'conf.interceptors'
+        ])
+        .config(initProvider);
 
-   ///////
+    ///////
 
-   function initProvider($urlRouterProvider, $compileProvider, $logProvider, $httpProvider) {
-      $urlRouterProvider.otherwise('/');
-      $compileProvider.debugInfoEnabled(false);
-      $logProvider.debugEnabled(true);
-      $httpProvider.interceptors.push('myHttpInterceptor');
-   }
+    function initProvider($urlRouterProvider, $compileProvider, $logProvider, $httpProvider) {
+        $urlRouterProvider.otherwise('/');
+        $compileProvider.debugInfoEnabled(false);
+        $logProvider.debugEnabled(true);
+        $httpProvider.interceptors.push('myHttpInterceptor');
+    }
 
-   initProvider.$inject = ['$urlRouterProvider', '$compileProvider', '$logProvider', '$httpProvider'];
+    initProvider.$inject = ['$urlRouterProvider', '$compileProvider', '$logProvider', '$httpProvider'];
 
 })();
