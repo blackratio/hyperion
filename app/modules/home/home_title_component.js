8c3a72776b5e8f52a5709d96d8c60e6c2b3b869a
blackratio <blackratio@gmail.com>
2016-04-21 22:54:21 +0200

    minor change, functions name etc

--- app/modules/home/home_title_component.js
+++ app/modules/home/home_title_component.js
@@ -2,24 +2,30 @@
 
    'use strict';
 
+   ////////
+
+   const loadComponent = {
+      bindings: {
+         title: '=',
+         user: '='
+      },
+      template: function () {
+         return `
+            <div class="title_content">
+               <h1>{{::$ctrl.title.title}}</h1>
+               <h2>{{::$ctrl.title.subtitle}}</h2>
+               <div class="gituser">
+                  <h3>{{::$ctrl.user.login}}</h3>
+                  <h4><h3>{{::$ctrl.user.url}}</h4>
+            </div>
+         `;
+      }
+   };
+
+   ////////
+
    angular
       .module('homeWelcome')
-      .component('homeTitle', {
-         bindings: {
-            title: '=',
-            user: '='
-         },
-         template: function () {
-            return `
-               <div class="title_content">
-                  <h1>{{::$ctrl.title.title}}</h1>
-                  <h2>{{::$ctrl.title.subtitle}}</h2>
-                  <div class="gituser">
-                     <h3>{{::$ctrl.user.login}}</h3>
-                     <h4><h3>{{::$ctrl.user.url}}</h4>
-               </div>
-            `;
-         }
-      });
+      .component('homeTitle', loadComponent);
 
 })();
